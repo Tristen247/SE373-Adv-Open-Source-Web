c593a26023bc5e23b1b10be30e5dd1a771ccf42f
Tristen247 <139776061+Tristen247@users.noreply.github.com>
2025-03-05 18:45:48 -0500

    Finished Week 3 Demo Code

--- WK1/app.js
+++ WK1/app.js
@@ -3,10 +3,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const exphbs = require("express-handlebars");
 const path = require('path');
 const fs = require('fs');
 const PORT = 3000;
 const app = express();
+
+//Set Handlebars as our templating engine
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -39,10 +45,67 @@ app.get("/games", async (req,res)=>{
         const games = await Game.find();
         res.json(games);
     }catch(err){
-        res.status(500).json({error:"Failed to fetch game data"})
+        res.status(500).json({error:"Failed to fetch game data"});
     }
 });
 
+// route to get game by ID (GET)
+app.get("/games/:id", async (req, res)=>{
+  try{
+      const game = await Game.findById(req.params.id);
+      if(!game){
+        return res.status(404).json({error:"Game not found"});
+      }
+      res.json(game)
+  }catch(err){
+      res.status(500).json({error:"Failed to fetch game data"});
+  }
+})
+
+// route to add game (POST)
+app.post("/addgame", async (req, res)=>{
+  try{
+      const newGame = new Game(req.body);
+      const savedGame = await newGame.save();
+      res.status(201).json(savedGame)
+      console.log(savedGame);
+  }catch(err){
+      res.status(400).json({error: "Failed to create entry"});
+  }
+})
+
+// route for update (PUT)
+app.put("/updategame/:id", async (req, res)=>{
+    //example using a promise statement
+    Game.findByIdAndUpdate(req.params.id, req.body, {
+      new:true,
+      runValidators:true
+    }).then((updatedgame)=>{
+        if(!updatedgame){
+          return res.status(400).json({error: "game not found"});
+          res.json(updatedgame);
+        }
+    }).catch((err)=>{
+      return res.status(400).json({error: "failed to update game"});
+    });     
+});
+
+// delete route (DELETE)
+app.delete("/deletegame/gamename/", async (req,res)=>{
+  try{
+      const gamename = req.query;
+      const game = await Game.find(gamename);
+
+      if(game.length === 0){
+          return res.status(404).json({ error: "Failed to find the game" });
+      }
+      const deletedGamme = await Game.findOneAndDelete(gamename);
+      res.json({message:"Game deleted successfully"})
+  }catch(err){
+      console.error(err);
+      res.status(404).json({ error: "Game not found"});
+  }
+})
 
 const readFile = (path) => {
   return new Promise((resolve, reject) => {
@@ -68,6 +131,10 @@ app.get('/todo', async (req, res) => {
 app.get('/read-todo', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'read-todo.html'));
 });
+
+app.get("/nodemon",(req,res)=>{
+  res.sendStatus(500);
+})
 
 // updated
 app.use((req, res) => {
