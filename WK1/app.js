9a3e0565f1489f2c52db95708af18dc0a66676ee
Tristen247 <139776061+Tristen247@users.noreply.github.com>
2025-03-04 22:51:33 -0500

    Installed Mongoose and Nodemon, Added Mongo DB Connections.

--- WK1/app.js
+++ WK1/app.js
@@ -1,4 +1,8 @@
+//Note: If something breaks or isnt working may need to check Express
+
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
 const PORT = 3000;
@@ -7,6 +11,38 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Middleware body-parser parses json reqs
+app.use(bodyParser.json());
+//MongoDB Database connection
+const mongoURI = "mongodb://localhost:27017/gamelibrary"
+mongoose.connect(mongoURI);
+const db = mongoose.connection;
+
+//check conection
+db.on("error", console.error.bind(console, "MongoDB Connection error"));
+db.once("open", ()=>{
+  console.log('connected to MongoDB Database')
+});
+
+//Mongoose Schema and Model
+const gameSchema = new mongoose.Schema({
+  gamename:String,
+  developer:String
+})
+
+const Game = mongoose.model("Game", gameSchema, "favoritegames");
+
+//Crud app examples Connection to DB route examples below
+app.get("/games", async (req,res)=>{
+    try{
+        const games = await Game.find();
+        res.json(games);
+    }catch(err){
+        res.status(500).json({error:"Failed to fetch game data"})
+    }
+});
+
 
 const readFile = (path) => {
   return new Promise((resolve, reject) => {
@@ -40,7 +76,7 @@ app.use((req, res) => {
   });
   res.end();
 });
-
+//PORT log
 app.listen(PORT, () => {
   console.log("Server running on port 3000.");
 });
